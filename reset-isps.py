7a121af47719aaafaf7926ff2ab16f5d739db30e
Kamalika Poddar <39147455+kamalikap@users.noreply.github.com>
2019-02-04 19:17:37 -0800

    Delete reset-isps.py

--- reset-isps.py
+++
@@ -1,9 +0,0 @@
-import json
-from subprocess import call
-
-with open('.ask/config') as f:
-    data = json.load(f)
-
-for x in data["deploy_settings"]["default"]["in_skill_products"]:
-    print x["filePath"].replace("isps/entitlement/", "").replace("isps/subscription/", "").replace("isps/consumable/", "")
-    call(["ask", "api", "reset-isp-entitlement", "-i", x["productId"]])
